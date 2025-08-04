92a859b66c2bd204d6951c7312ee1779397e8a35
aleksei-ojegov <aleksei.ojegov@yandex.ru>
2025-08-04 23:49:21 +0400

    Adding a starter library for a telegram bot
    Первая версия библиотеки с функцией эхо

---
+++ TelegramBot/Bot/Program.cs
@@ -0,0 +1,15 @@
+﻿using TiskTask.TelegramBot;
+
+Console.WriteLine("Запуск Telegram-бота...");
+
+var cts = new CancellationTokenSource();
+
+// 🔐 Замени на свой токен
+const string BotToken = "---";
+
+var botService = new TelegramBot(BotToken);
+
+// Запускаем бота
+await botService.StartAsync(cts.Token);
+
+Console.WriteLine("Бот остановлен.");
