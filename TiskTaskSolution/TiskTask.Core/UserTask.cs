4b47cb6197424800978b078545a16ad754dffe5a
elgrenka <bugigen@yandex.ru>
2025-08-06 19:54:52 +0400

    add documentation to UserTask.cs

--- TiskTaskSolution/TiskTask.Core/UserTask.cs
+++ TiskTaskSolution/TiskTask.Core/UserTask.cs
@@ -11,11 +11,34 @@ namespace TiskTask.Core
     /// </summary>
     public class UserTask
     {
+        /// <summary>
+        /// Идентификатор.
+        /// </summary>
         public int Id { get; private set; }
+
+        /// <summary>
+        /// Идентификатор пользователя telegram.
+        /// </summary>
         public int UserId { get; private set; }
+
+        /// <summary>
+        /// Название задачи.
+        /// </summary>
         public string Title { get; private set; }
+
+        /// <summary>
+        /// Описание задачи.
+        /// </summary>
         public string Description { get; private set; }
+
+        /// <summary>
+        /// Дата создания задачи.
+        /// </summary>
         public DateTime Created { get; private set; }
+
+        /// <summary>
+        /// Потраченное время на задачу.
+        /// </summary>
         public TimeSpan TimeSpent {  get; private set; }
     }
 }
